7b51a460f5b8f0f4f12f92a3f769cc9d567c7db5
TommyAudet <tommyaudet@magame.ca>
2020-03-14 15:42:52 -0400

    ajout de déplacement d'icones gauche droite

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo "Sorry, your file was not uploaded. </br>";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -63,7 +63,7 @@ if ($handle = opendir('uploads')) {
  
     foreach($images2 as $image2)
     {
-      echo $image2.'<br/>'; // List all Images
+      echo "<input value='$image2'>" . '<br/>'; // List all Images
     }
     closedir($handle);
 }
@@ -75,60 +75,77 @@ if ($handle = opendir('uploads')) {
 
 <!DOCTYPE html>
 <html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        
-        
-            
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">     
         <!--font awesome-->
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
             
         <!--feuille css externe qui link le font dans canvas    -->
         <link rel="stylesheet" type="text/css" href="style.css">
-        
-        
+         
          <!-- Required meta tags -->
-    
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     </head>
+    
 <body>
+    <div class="alert alert-primary w-100"   style="visibility: hidden; position: fixed; top: 1em; z-index: 5;" id="alert">
+        Liste Des Hashtags copié
+    </div>
+    
     <form action="" method="post" enctype="multipart/form-data">
             Select image to upload:
         <input type="file" name="fileToUpload" id="fileToUpload" class="">
         <input type="submit" value="Upload Image" name="submit"class="btn btn-primary" >
     </form>
     
-    
     <div class="container-fluid">
-        
-    <input type="text" id="input">
-    <button id="submit" class="btn btn-primary">cliquez 2 fois pour générer thumbnail</button>
+        <input type="text" id="input">
+        <button id="submit" class="btn btn-primary">cliquez 2 fois pour générer thumbnail</button>
     
-<div>
-<img id="scream" src="" style="width: 10%; height: auto;" crossorigin>
-<img id="scream2" src="icons2.png" style="width: 1%; height: auto;" crossorigin>
+            <div>
+                <img id="scream" src="" style="width: 10%; height: auto;" crossorigin>
+                <img id="scream2" src="icons2.png" style="width: 1%; height: auto;" crossorigin>
+    
+            </div>
 
-</div>
+            <div>
+                <div class="row">
+                    <canvas id="myCanvas" height="100" width="100" style="border:1px solid #000000; position: relative;" class="mx-auto">
+                    </canvas>
+            </div>
+            <div class="row">
+                <button id="save" class="btn btn-primary mx-auto m-3">save</button>
+            </div>
+        
+        <p style="font-family : ARCADE; color: yellow; font-size: 3rem;">shit</p>
+        </div>
+        <div class="row">
+            <div class=" mx-auto">
+            <button id="gauche" class="btn btn-outline-primary mx-auto">gauche</button>
+            <button id="droite" class="btn btn-outline-primary mx-auto">droite</button>
+        
+            <button id="haut" class="btn btn-outline-primary mx-auto">haut</button>
+            <button id="bas" class="btn btn-outline-primary mx-auto">bas</button>
+            </div>
+        </div>
+    </div>
 
-<div>
+<div class="container-fluid">
     <div class="row">
-<canvas id="myCanvas" height="100" width="100" style="border:1px solid #000000; position: relative;" class="mx-auto">
-
-
-
-</canvas>
-  </div>
-<p style="font-family : ARCADE; color: yellow; font-size: 3rem;">shit</p>
-
+        <p id="hashtags" class="col-6 mx-auto">
+            pour télécharger un podcast version audio ou lire des articles sur l’actualité gaming, rend toi au www.MaGame.ca
+            #gaming #jeuxvideo #qc #quebec #quebecois #podcast #balado #actualité #MaGame #pc #gamingpc #xbox #xboxone #microsoft #sony #playstation #playstation4 #ps4 #nintendo #nintendoswitch #pcmasterrace #jeux #canadien #canada #angrybirds #rovio #epicgames #epicgamestore #overwatch #scarlettjohansson
+        </p>
+    </div>
+    <div class="row">
+        <button id="copyHash" class="btn btn-primary mx-auto " >copie #</button>
+    </div>
 </div>
 
-<button id="save" class="btn btn-primary" >save</button>
 
-  
-</div>
+
 
 <script
 			  src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -181,30 +198,84 @@ var HeightIcons = document.getElementById('scream2').height;
   var ctx = c.getContext("2d");
   var img = document.getElementById("scream");
   
+  
+      ctx.drawImage(img, 0, 0);
+    ctx.drawImage(document.getElementById("scream2"), percentPaddingLeft, percentPaddingTop, percentWidthIcons, percentHeightIcons);
+    ctx.shadowColor="black";
+    ctx.shadowBlur=5;
+    ctx.lineWidth=5;
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'rgb(255, 170, 0)';
+    ctx.font = "4.5rem ARCADE";
+    ctx.fillText("MaGame Podcast", Width/2, 60); 
+    ctx.font = "2.5rem ARCADE";
+    ctx.fillText("en direct les mercredis 21h15", Width/2, 90); 
+  
    
   
-  ctx.drawImage(img, 0, 0);
-  ctx.drawImage(document.getElementById("scream2"), percentPaddingLeft, percentPaddingTop, percentWidthIcons, percentHeightIcons);
-  
-  
-  ctx.shadowColor="black";
-ctx.shadowBlur=7;
-ctx.lineWidth=5;
-  ctx.textAlign = 'center';
-  ctx.fillStyle = 'yellow';
-ctx.font = "3rem ARCADE";
-ctx.fillText("MaGame Podcast", Width/2, 50); 
-ctx.font = "1.5rem ARCADE";
-ctx.fillText("en direct les mercredis 21h15", Width/2, 90); 
 
 
 
-  
-  
+
+   var deplacementGauche = percentPaddingLeft;
+ var deplacementTop = percentPaddingTop;
  
-   
+ var newPositionLeftRight;
+  var newPositionUpDown;
+ 
+    $("#gauche").click(function(){
+       console.log("shit"); 
+       percentPaddingLeft - (deplacementGauche -= 8);
+       newPositionLeftRight = deplacementGauche;
+       
+       drawLeftRight(img, Width, Height, WidthIcons, HeightIcons, percentWidthIcons, percentHeightIcons, newPositionLeftRight, percentPaddingTop, c, ctx);
+       
+    });
+    
+    $("#droite").click(function(){
+       console.log("shit"); 
+       percentPaddingLeft - (deplacementGauche += 8);
+       newPositionLeftRight = deplacementGauche;
+       
+       drawLeftRight(img, Width, Height, WidthIcons, HeightIcons, percentWidthIcons, percentHeightIcons, newPositionLeftRight, percentPaddingTop, c, ctx);
+    });
+    
+    /*$("#haut").click(function(){
+       console.log("shit"); 
+       draw(img, Width, Height, WidthIcons, HeightIcons, percentWidthIcons, percentHeightIcons, percentPaddingLeft , percentPaddingTop - (deplacementTop += 8), c, ctx, newPositionLeftRight, newPositionUpDown);
+       newPositionUpDown = deplacementTop;
+    });
+    
+    $("#bas").click(function(){
+       console.log("shit"); 
+       draw(img, Width, Height, WidthIcons, HeightIcons, percentWidthIcons, percentHeightIcons, percentPaddingLeft , percentPaddingTop - (deplacementTop -= 8), c, ctx, newPositionLeftRight, newPositionUpDown);
+       newPositionUpDown = deplacementTop;
+    });*/
+
    
 });
+
+
+
+
+
+function drawLeftRight(img, Width, Height, WidthIcons, HeightIcons, percentWidthIcons, percentHeightIcons, newPositionLeftRight, percentPaddingTop, c, ctx, ){
+    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(document.getElementById("scream2"), newPositionLeftRight, percentPaddingTop, percentWidthIcons, percentHeightIcons);
+    ctx.shadowColor="black";
+    ctx.shadowBlur=5;
+    ctx.lineWidth=5;
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'rgb(255, 170, 0)';
+    ctx.font = "4.5rem ARCADE";
+    ctx.fillText("MaGame Podcast", Width/2, 60); 
+    ctx.font = "2.5rem ARCADE";
+    ctx.fillText("en direct les mercredis 21h15", Width/2, 90); 
+}
+
+
+
+
 $("#save").click(function(){
      var canvas = document.getElementById("myCanvas");
   image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
@@ -214,6 +285,24 @@ $("#save").click(function(){
   link.click();
 });
 
+
+$("#copyHash").click(function(){
+    $('#alert').css("visibility", "visible");
+    
+    let hashtags = $("#hashtags").text();
+    textToClipboard(hashtags);
+			function textToClipboard (text) {
+				var dummy = document.createElement("textarea");
+				document.body.appendChild(dummy);
+				dummy.value = text;
+				dummy.select();
+				document.execCommand("copy");
+				document.body.removeChild(dummy);
+				
+				setTimeout(function(){ $('#alert').css("visibility", "hidden"); }, 2000);
+			}
+			
+});
 
 
 </script>
